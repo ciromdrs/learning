d6a2283f8dbc4a094ce8eb7b141ba84d16ba664f
Ciro <ciromoraismedeiros@gmail.com>
2018-02-05 09:13:46 -0300

    Finished basics

--- go/basics/packages.go
+++ go/basics/packages.go
@@ -66,4 +66,21 @@ func main() {
     var f float64 = math.Sqrt(float64(x*x + y*y))
     var zz uint = uint(f)
     fmt.Println(x, y, zz)
+    
+    const (
+        Big = 1 << 100
+        Small = Big >> 99
+    )
+    
+    fmt.Println(needInt(Small))
+    fmt.Println(needFloat(Small))
+    fmt.Println(needFloat(Big))
+}
+
+func needInt(x int) int {
+    return x*10 + 1
+}
+
+func needFloat(x float64) float64 {
+    return x * 0.1
 }
